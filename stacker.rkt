933bd366e13059d79c5a7ffbebf0182d378adf42
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2025-06-23 22:12:30 +0000

    upto module-begin

---
+++ stacker.rkt
@@ -0,0 +1,13 @@
+#lang br/quicklang
+
+(define (read-syntax path port)
+  (define src-lines (port->lines port))
+  (define src-datums (format-datums '(handle ~a) src-lines))
+  (define module-datum
+    `(module stacker-mod "stacker.rkt"
+       ,@src-datums))
+  (datum->syntax #f module-datum))
+(provide read-syntax)
+
+(define-macro (stacker-module-begin HANDLE-EXPR ...) #'(#%module-begin HANDLE-EXPR ...))
+(provide (rename-out [stacker-module-begin #%module-begin]))
