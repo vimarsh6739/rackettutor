7d5e4c1c0e5bb5a9f617f6101cf49438111c1468
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2025-06-30 17:09:13 +0000

    added bf, lexer, stub parser/expander

---
+++ bf/reader.rkt
@@ -0,0 +1,24 @@
+#lang br/quicklang
+(require "parser.rkt")
+
+(define (read-syntax path port)
+  (define parse-tree (parse path (make-tokenizer port)))
+  (define module-datum `(module bf-mod "expander.rkt" 
+                          ,parse-tree))
+  (datum->syntax #f module-datum))
+
+(provide read-syntax)
+
+;; define the lexer (use brag/support)
+
+(require brag/support)
+(define (make-tokenizer port)
+  (define (next-token)
+    (define bf-lexer
+      (lexer
+       [(char-set "><-.,+[]") lexeme]
+       [any-char (next-token)]))
+    (bf-lexer port))  
+  next-token)
+
+
