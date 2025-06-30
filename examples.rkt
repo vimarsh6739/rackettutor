68288d510b76e1b85e28e8b98c9c4bfb5a7a0a5a
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2025-06-30 16:07:24 +0000

    macro macro macro

--- examples.rkt
+++ examples.rkt
@@ -1,4 +1,25 @@
 #lang racket
-
+(require br/macro)
+;; simple for loop with fold(l)
 (for/fold ([sum 0]) ([int (list 1 2 3 4 5 6 7 8 9)])
   (+ sum int))
+
+;; messing around with ...
+;; ... is a placeholder for ARG here 
+(define-macro (lister ARG ...)
+  #'(list ARG ...))
+
+(lister "foo" "bar" "baz")
+; '("foo" "bar" "baz")
+
+(define-macro (wrap ARG ...)
+  #'(list '(ARG 42) ...))
+
+(wrap "foo" "bar" "baz")
+; '(("foo" 42) ("bar" 42) ("baz" 42))
+
+(define-macro (wrap2 ARG ...)
+  #'(list '(ARG 42 ARG) ...))
+
+(wrap2 "foo" "bar" "baz")
+; '(("foo" 42 "foo") ("bar" 42 "bar") ("baz" 42 "baz"))
--- stackerizer.rkt
+++ stackerizer.rkt
@@ -8,11 +8,11 @@
 
 (provide (rename-out [stackerizer-mb #%module-begin]))
 
-(define-macro (define-op OP)
-#'(define-macro-cases OP 
-  [(OP FIRST) #'FIRST] 
-  [(OP FIRST NEXT (... ...)) #'(list 'OP FIRST (OP NEXT (... ...)))]))
+(define-macro (define-ops OP ...)
+#'(begin 
+    (define-macro-cases OP 
+      [(OP FIRST) #'FIRST] 
+      [(OP FIRST NEXT (... ...)) #'(list 'OP FIRST (OP NEXT (... ...)))])
+    ...))
 
-(define-op +)
-(define-op *)
-
+(define-ops + *)
