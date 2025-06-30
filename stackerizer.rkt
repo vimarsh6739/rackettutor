438e95aa4883dcca9e1eff488c98a3d4a53f2b31
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2025-06-30 15:43:18 +0000

    stackerizer with only +

---
+++ stackerizer.rkt
@@ -0,0 +1,14 @@
+#lang br/quicklang
+(provide *
+         +)
+
+(define-macro (stackerizer-mb EXPR) 
+  #'(#%module-begin 
+     (for-each displayln (reverse (flatten EXPR)))))
+
+(provide (rename-out [stackerizer-mb #%module-begin]))
+
+(define-macro-cases + 
+  [(+ FIRST) #'FIRST] 
+  [(+ FIRST NEXT ...) #'(list '+ FIRST (+ NEXT ...))])
+
